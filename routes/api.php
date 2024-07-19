7d14c737831be50caf53325dadb6ffb41f84c668
Sandip Shrestha <sandipshrestha2052@gmail.com>
2024-07-19 09:08:13 +0545

    postman

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,8 @@ Route::get('/', function () {
     );
 });
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
